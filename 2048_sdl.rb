4bb5bb4bbf7d702dd38e7a946a677fe37d488d12
cascassette <casper.ravenhorst@gmail.com>
2015-11-08 19:11:07 +0100

    fully documented in code

--- 2048_sdl.rb
+++ 2048_sdl.rb
@@ -28,47 +28,17 @@ require 'color'
 include Rubygame
 
 class Game
-	# Size of a block on screen
-	BLKSIZE = 100
-	# Size of a statistics block on screen
-	STATBLKOUTERWIDTH = 25
-	# Size of a statistics block on screen
-	STATBLKINNERWIDTH = 22
-
-	# ORIENTATIONS hash is used to translate 4
-	# swiping directions into symbols :col or :row
-	# See +shake_board+, +translate_arrays+, +shake+
-	ORIENTATIONS = {
-		north: :col,
-		south: :col,
-		east:  :row,
-		west:  :row
-	}
-
-	# DIRECTIONS hash is used to translate 4
-	# swiping directions into symbols :pos or :neg
-	# See shake_board, translate_arrays, shake
-	DIRECTIONS = {
-		north: :pos,
-		west:  :pos,
-		south: :neg,
-		east:  :neg
-	}
-
-	KEY_TRANSLATE = {
-		left:  :west,
-		right: :east,
-		up:    :north,
-		down:  :south
-	}
-
+	# Initializes the games internal, as well as
+	# the frontend representation for the game
+	# Options may contain a Hash in the form:
+	#     { board_size: 5 }
 	def initialize (options)
+		# If undefined, @board_size will default to 4
 		@board_size = options[:board_size] || 4
 
-		@empty_spots = Array.new
-		@stat_block = nil
 		@board = Array.new(@board_size**2) { 0 }
 		@moves = 0
+		@collisions = 0
 		@stats_sorted = [ [ "   2", [ 1, 1 ] ] ]
 
 		TTF.setup
@@ -106,43 +76,116 @@ class Game
 		@event_queue.enable_new_style_events
 	end
 
+	# Launches one game
+	def launch
+		generate_new
+		while true
+			show_board
+			if inp = get_input
+				shake_board inp
+			else
+				break
+			end
+			if @move_made
+				if not generate_new
+					break
+				end
+				update_stats
+			end
+		end
+		Rubygame.quit
+		game_over
+	end
+
+	private
+	# Size of a block on screen
+	BLKSIZE = 100
+	# Size of a statistics block on screen (shows
+	# how many of a block type you have)
+	STATBLKOUTERWIDTH = 25
+	# Size of a statistics block on screen (shows
+	# how many of a block type you have)
+	STATBLKINNERWIDTH = 22
+
+	# ORIENTATIONS hash is used to translate 4
+	# swiping directions into symbols :col or :row
+	# See shake_board, translate_arrays, shake
+	ORIENTATIONS = {
+		north: :col,
+		south: :col,
+		east:  :row,
+		west:  :row
+	}
+
+	# DIRECTIONS hash is used to translate 4
+	# swiping directions into symbols :pos or :neg
+	# See shake_board, translate_arrays, shake
+	DIRECTIONS = {
+		north: :pos,
+		west:  :pos,
+		south: :neg,
+		east:  :neg
+	}
+
+	# KEY_TRANSLATE simply points the key names
+	# from Rubygames Events::KeyPressed event
+	# to this games names for the swipe directions
+	KEY_TRANSLATE = {
+		left:  :west,
+		right: :east,
+		up:    :north,
+		down:  :south
+	}
+
+	# Lets you set a board positions tile value
 	def set_x_y (x, y, val)
-		pos = map_x_y(i)
+		pos = map_x_y(x, y)
 		if is_in_range? pos
 			@board[pos] = val
 		end
 	end
 
+	# Maps from [(1..@board_size),(1..@board_size)] to (0..@board_size**2-1)
+	# ie from [1..4] x and y coordinates to the flat array form
 	def map_x_y (x, y)
 		(x-1)*@board_size+y-1
 	end
 
+	# Maps back from (0..@board_size**2-1) to [(1..@board_size),(1..@board_size)]
+	# ie from flat array index to [1..4] x and y coordinates
 	def map_i (i)
 		[(i % @board_size) + 1, (i / @board_size) + 1]
 	end
 
+	# Returns the tile value of a [x, y] position
+	# on the board
 	def get_board (x, y)
-		if x >= 1 && y >= 1 && x <= @board_size && y <= @board_size then
-			@board[(x-1)*@board_size+y-1] || 0
+		pos = map_x_y(x, y)
+		if is_in_range? pos
+			@board[(x-1)*@board_size+y-1]
 		else
 			nil
 		end
 	end
 
+	# Tells you whether the given array index i
+	# is in the boards flat range (0..@board_size**2-1)
 	def is_in_range? (i)
 		i >= 0 && i < (@board_size**2)
 	end
 
+	# Updates the screen with the current state of
+	# the board and statistics
 	def show_board
 		# draw stats
 		@empty_surface.blit @screen, @empty_surface.make_rect
 		@stats_sorted.each do |a|
 			tileno, amt = a[1][0], a[1][1]
-			@stat_block = Surface.new [STATBLKINNERWIDTH, amt*2]
-			@stat_block.draw_box_s [0, 0], [STATBLKINNERWIDTH, amt*2], @colors[tileno]
-			rect = @stat_block.make_rect
+			stat_block = Surface.new [STATBLKINNERWIDTH, amt*2]
+			stat_block.draw_box_s [0, 0], [STATBLKINNERWIDTH, amt*2], @colors[tileno]
+			rect = stat_block.make_rect
 			rect.topleft = [ 2+STATBLKOUTERWIDTH*tileno, 2 ]
-			@stat_block.blit @screen, rect
+			stat_block.blit @screen, rect
 		end
 
 		# draw tiles
@@ -160,18 +203,21 @@ class Game
 		@screen.flip
 	end
 
+	# Generates a new '2' tile on an empty spot
 	def generate_new
-		@empty_spots.clear
-		@board.each_with_index { |tile, i| @empty_spots.push(i) if tile == 0 }
-		if @empty_spots.length == 0		# game over detection still fails.. need to check if moves can be made
+		empty_spots = Array.new
+		@board.each_with_index { |tile, i| empty_spots.push(i) if tile == 0 }
+		if empty_spots.length == 0		# game over detection still fails.. need to check if moves can be made
 			return false
 		else
-			place = rand(0..@empty_spots.length-1)
-			@board[@empty_spots[place]] = 1
+			place = rand(0..empty_spots.length-1)
+			@board[empty_spots[place]] = 1
 		end
 		true
 	end
 
+	# Waits for user input in the form of
+	# arrow keys or <Esc>
 	def get_input
 		res = nil
 		while event = @event_queue.wait
@@ -188,6 +234,13 @@ class Game
 		res
 	end
 
+	# Permutates the board according to ORIENTATIONS
+	# and DIRECTIONS symbols, such that the array of
+	# arrays returned can be viewed as rows shifting
+	# left (before being permutated back to original
+	# form)
+	# Returns a @board_size by @board_size matrix
+	# (array of arrays)
 	def translate_arrays(orientation, direction)
 		bs = @board_size
 		res = Array.new(bs) { Array.new(bs) {0} }
@@ -203,6 +256,9 @@ class Game
 		res
 	end
 
+	# Takes an array of arrays as delivered by
+	# translate_arrays, and turns it back into the
+	# original
 	def untranslate_arrays(b, orientation, direction)
 		bs = @board_size
 		res = Array.new (bs**2)
@@ -218,6 +274,8 @@ class Game
 		res
 	end
 
+	# Shifts a row to the left (in the direction of
+	# array index 0)
 	def shake(ary)
 		bs = @board_size
 		used_in_collision = Array.new(bs) { false }
@@ -238,6 +296,7 @@ class Game
 					ary[searchleft] += 1
 					used_in_collision[searchleft] = true
 					ary[i] = 0
+					@collisions += 1
 					@move_made = true
 				elsif collide_value != 0 and searchleft+1 != i
 					ary[searchleft+1] = ary[i]
@@ -253,6 +312,10 @@ class Game
 		ary
 	end
 
+	# Encapsulates translate_arrays, shake, and
+	# untranslate_arrays to do one move
+	# movement should be one of :north, :east,
+	# :south and :west
 	def shake_board(movement)
 		orientation = ORIENTATIONS[movement]
 		direction   = DIRECTIONS[movement]
@@ -267,6 +330,9 @@ class Game
 		@board = untranslate_arrays(workboard, orientation, direction)
 	end
 
+	# Keeps a hash, @stats_sorted, up to date
+	# with info on how many times each tile is
+	# on the board
 	def update_stats
 		stats = Hash.new (0)
 		@board.each do |tile|
@@ -278,9 +344,8 @@ class Game
 		@stats_sorted = stats.sort_by {|k, v| v[0]}
 	end
 
+	# Prints final statistics
 	def game_over
-		puts "Your highest tile was: " + (2**@board.max).to_s
-		puts "Moves: " + @moves.to_s
 		score = (@board.inject { |sum, x| sum + ((x>0)?(2**x):0) })
 		print "Your score: " + score.to_s
 		if score > $highscore
@@ -291,27 +356,13 @@ class Game
 		else
 			puts "  (highscore %d)" % $highscore
 		end
-		puts "Tiles:"
-		@stats_sorted.each { |a| puts "%s: %2d" % [a[0], a[1][1]] }
-	end
-
-	def launch
-		generate_new
-		while true
-			show_board
-			if inp = get_input
-				shake_board inp
-			else
-				break
-			end
-			if @move_made
-				if not generate_new
-					break
-				end
-				update_stats
-			end
-		end
-		game_over
+		puts "---"
+		puts "Statistics time"
+		puts "Your highest tile was: " + (2**@board.max).to_s
+		puts "Moves: " + @moves.to_s
+		puts "Average number of collisions per move: %6.3f (%d collisions)" % [ (@collisions.to_f / @moves), @collisions ]
+		puts "Distribution of tiles:"
+		@stats_sorted.each { |a| puts "%s: %s" % [a[0], "|" * a[1][1]] }
 	end
 end
 
@@ -321,8 +372,5 @@ if __FILE__ == $0
 
 
 	game = Game.new board_size: 4
-	puts
 	game.launch
-
-	Rubygame.quit
 end
